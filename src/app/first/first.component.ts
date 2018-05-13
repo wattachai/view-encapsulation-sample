f2d0594c8cbcd290d937eae5d12245d7b0889b16
Wattachai Kanawitoon <wattachai@users.noreply.github.com>
2018-05-13 10:51:22 +0700

    set style to start of demo state

--- src/app/first/first.component.ts
+++ src/app/first/first.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   selector: 'app-first',
   templateUrl: './first.component.html',
   styleUrls: ['./first.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  // encapsulation: ViewEncapsulation.None
 })
 export class FirstComponent implements OnInit {
 
